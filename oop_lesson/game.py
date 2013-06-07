3024f22a10856fbebc24e5959f008e5f1942b323
cdueltgen <cdueltgen@gmail.com>
2013-06-06 17:58:02 -0700

    up to Step 14

--- oop_lesson/game.py
+++ oop_lesson/game.py
@@ -17,12 +17,64 @@ GAME_HEIGHT = 5
 #### Put class definitions here ####
 class Rock(GameElement):
     IMAGE = "Rock"
+    SOLID = True
+
+class Character(GameElement):
+    IMAGE = "Girl"
+
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+        return None
+
+class Gem(GameElement):
+    IMAGE = "BlueGem"
+    SOLID = False
+
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!"%(len(player.inventory)))
 ####   End class definitions    ####
+
+def keyboard_handler():
+    direction = None
+
+    if KEYBOARD[key.UP]:
+        direction = "up"
+    if KEYBOARD[key.DOWN]:
+        direction = "down"
+    if KEYBOARD[key.LEFT]:
+        direction = "left"
+    if KEYBOARD[key.RIGHT]:
+        direction = "right"
+
+    if direction:
+        next_location = PLAYER.next_pos(direction)
+        next_x = next_location[0]
+        next_y = next_location[1]
+
+        existing_el = GAME_BOARD.get_el(next_x, next_y)
+        if existing_el:
+            existing_el.interact(PLAYER)
+
+        if existing_el is None or not existing_el.SOLID:
+            GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
+            GAME_BOARD.set_el(next_x, next_y, PLAYER)
 
 def initialize():
     """Put game initialization code here"""
 
-    # Initalize and register rock1
+    # Initalize and register rocks
     rock_positions = [(2,1), (1,2), (3,2), (2,3)]
     rocks = []
 
@@ -32,5 +84,22 @@ def initialize():
         GAME_BOARD.set_el(pos[0], pos[1], rock)
         rocks.append(rock)
 
+    rocks[-1].SOLID = False
+
     for rock in rocks:
         print rock
+
+    # Initalize and register a player
+    global PLAYER
+    PLAYER = Character()
+    GAME_BOARD.register(PLAYER)
+    GAME_BOARD.set_el(2, 2, PLAYER)
+    print PLAYER
+
+    # Print a message to the screen
+    GAME_BOARD.draw_msg("This game is wicked awesome.")
+
+    # Initalize and register a gem
+    gem = Gem()
+    GAME_BOARD.register(gem)
+    GAME_BOARD.set_el(3, 1, gem)
