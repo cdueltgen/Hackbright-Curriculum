b10bd2320938811c081665dde78f3a6dcd92a330
cdueltgen <cdueltgen@gmail.com>
2013-06-06 17:18:57 -0700

    initial commit, game.py through Step 4

---
+++ oop_lesson/game.py
@@ -0,0 +1,36 @@
+import core
+import pyglet
+from pyglet.window import key
+from core import GameElement
+import sys
+
+#### DO NOT TOUCH ####
+GAME_BOARD = None
+DEBUG = False
+KEYBOARD = None
+PLAYER = None
+######################
+
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
+
+#### Put class definitions here ####
+class Rock(GameElement):
+    IMAGE = "Rock"
+####   End class definitions    ####
+
+def initialize():
+    """Put game initialization code here"""
+
+    # Initalize and register rock1
+    rock_positions = [(2,1), (1,2), (3,2), (2,3)]
+    rocks = []
+
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+
+    for rock in rocks:
+        print rock
